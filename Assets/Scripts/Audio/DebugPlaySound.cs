61e3a9f8ad8d4530805988580176ea9dbd8bdf0f
Dell XPS\Leonardo <leonardo.fiori.1996@gmail.com>
2018-05-28 21:42:56 +0200

    Sound manager
    creato prefab sound manager.
    attenzione è un singleton.
    aggiunto debug player con tasto in inspector
    spostati alcuni script nella cartella giusta

---
+++ Assets/Scripts/Audio/DebugPlaySound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugPlaySound : MonoBehaviour {
+
+    public string soundName;
+    public float volume;
+    
+    public void Play()
+    {
+        SoundManager.instance.Play(soundName, volume);
+    }
+}
