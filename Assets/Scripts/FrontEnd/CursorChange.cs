bcfae04b61ca8f72176c7ff235e737f0312a4b2b
Dodoz <domizianoatticciati@gmail.com>
2018-06-08 14:54:07 +0200

    Cursore + prefab raymanager
    Creato script per il cursore
    Creato prefab del raycast manager
    aggiunte sprite cursore nella cartella hud
    cominciato fix turni( lo sto dicendo da 10 commit)

---
+++ Assets/Scripts/FrontEnd/CursorChange.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChange : MonoBehaviour
+{
+
+    public Texture2D newTextureCursor;
+
+ 
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag != "Tile") return;
+            TileMovement clickedTile = hit.transform.GetComponent<TileMovement>();
+            //print("Effettivamente è una tile ");
+
+
+            if((clickedTile.GetTileX() == GameManager.playerInstance.getX()) && (clickedTile.GetTileY() == GameManager.playerInstance.getY()))
+            {
+                //print("Tile del bambino");
+                ChangeCursor();
+            }
+            
+            else
+            {
+                ResetCursor();
+            }
+        }
+    }
+    // Update is called once per frame
+    void ChangeCursor()
+    {
+
+        //print("Cambio cursore");
+        Cursor.SetCursor(newTextureCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void ResetCursor()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
---
+++ Assets/Scripts/UI/ListTurniUI.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListTurniUI : MonoBehaviour {
+
+    public List<Image> images;    
+    public Image turnoAttivo;
+    public Image turnoDisattivato;
+    public Image scarpetta;
+
+    int j = 1;
+    // Use this for initialization
+    void Start () {
+
+        images = new List<Image>(new Image[GameManager.playerInstance.GetMossePerTurno()]);
+                          
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            images[i] = Instantiate(turnoAttivo, transform);
+        }
+
+        print(images.Count);
+        print(GameManager.playerInstance.GetMossePerTurno());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetKeyDown(KeyCode.K)) TurnoPermanente();
+        print(j);
+	}
+
+    public void ScalaTurno()
+    {
+        int turnoMassimo = GameManager.playerInstance.GetMossePerTurno();
+        
+        print("ScalaTurno() in posizione " + (turnoMassimo - j));   // faccio la sottrazione a causa di come viene tenuto il conteggio dei turni che non conterebbe lo 0
+
+        images[turnoMassimo - j].gameObject.SetActive(false);
+        GameObject.Destroy(images[turnoMassimo - j]);
+
+        images[turnoMassimo - j] = Instantiate(turnoDisattivato, transform);
+        j++;
+
+    }
+
+    //public void TurnoPermanente()
+    //{
+    //    print("Mossa permanente");
+    //    images.Insert(jInstantiate(turnoAttivo , transform));
+    //    images.
+    //    j--;
+    //}
+
+
+
+
+    public void ResetTurno()
+    {
+        print("ResetTurno() invocato, Resetto UI e conteggio ScalaTurno()");
+
+        int turnoMassimo = GameManager.playerInstance.GetMossePerTurno();
+
+        if (turnoMassimo == images.Count) print("La capacità delle mosse massime è arrivata al massimo");
+
+        for (int i = 0; i < turnoMassimo; i++) // 
+        {
+            images[i].gameObject.SetActive(false);
+            GameObject.Destroy(images[i]);
+            images[i] = Instantiate(turnoAttivo, transform); // attivo le prime 3                   
+        }
+
+
+
+        for (int i = turnoMassimo; i < images.Count; i++) // da 3 a 5 metto quelle non disponibili
+        {
+            images[i].gameObject.SetActive(false);
+            GameObject.Destroy(images[i]);
+            images[i] = Instantiate(turnoDisattivato, transform);   // disattivo quelle dopo
+        }
+
+        j = 1;
+    }
+}
