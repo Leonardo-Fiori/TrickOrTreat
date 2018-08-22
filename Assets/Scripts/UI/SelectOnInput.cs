69d4c771c65f1f50355d9d297a4e4381d9a5aff0
Dodoz <domizianoatticciati@gmail.com>
2018-08-22 17:06:32 +0200

    Ui fix
    Corretti gli scale di risoluzione ui
    Scroccato effetto fade di leo pure al menu main
    Aggiunto script che permette di navigare sul menu tramite tastiera (pure gamepad forse , not tested)

---
+++ Assets/Scripts/UI/SelectOnInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class SelectOnInput : MonoBehaviour
+{
+
+    // QUESTO SCRIPT SERVE A DETECTARE NEL MENU SE VIENE USATO KEYBOARD O GAMEPAD
+    public EventSystem eventSystem;
+    public GameObject selectedObject;   //il primo oggetto che selezionerà
+
+    private bool buttonSelected = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((Input.GetAxisRaw("Vertical")) != 0 && buttonSelected == false)
+        {  // se detecto qualsiasi movimento sul vertical axis da keyboard o gamepad(frecce su\giu e WS?)
+
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonSelected = false;     //?
+    }
+}
